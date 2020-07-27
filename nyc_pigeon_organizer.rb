d755fea11d5350d7ca6a3e3aca6ca5a60f0018fe
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-27 19:05:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_result = data.each_with_object ({}) do | (key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+          if !final_array[name][key]
+            !final_array[name][key] = []
+          end
+          final_array[name][key].push(inner_key.to_s)
+        end
+      end
+    end
 end
